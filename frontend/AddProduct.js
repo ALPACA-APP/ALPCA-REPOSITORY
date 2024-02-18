ca12e990780bb2651473708f2b8c91a22eb4afe3
Coding-Smurf <correoparaspam2003@gmail.com>
2024-02-18 21:52:46 +0100

    test on android if scanner is working

--- frontend/AddProduct.js
+++ frontend/AddProduct.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import { CameraView, Camera } from "expo-camera/next";
 
+
+
 import ScanStyles from './ScanStyles'; // Assuming you still need styles
 
 export default function Scan() {
@@ -17,17 +19,25 @@ export default function Scan() {
 
   if (hasPermission === null) {
     return <Text>No access to camera</Text>;
-  } else if (hasPermission === false) {
+  } else if (hasPermission === false) { 
     return <Text>No access to camera</Text>;
   }
 
-
+  const scanned = (data) => {
+    console.log(data);
+  };
 
 
   
   return (
     <View style={ScanStyles.absoluteFillObject}>
-      <CameraView style={ScanStyles.absoluteFillObject} />
+      <CameraView style={ScanStyles.absoluteFillObject} 
+
+          barCodeScannerSettings={{
+            barCodeTypes: ["qr" ],
+          }}
+          onBarCodeScanned={scanned}
+      />
     </View>
   );
 }
