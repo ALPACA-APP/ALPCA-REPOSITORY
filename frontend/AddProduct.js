56d13d6490616b63d2b2274a0d99f8346386f4e7
Coding-Smurf <correoparaspam2003@gmail.com>
2024-02-18 19:25:41 +0100

    added camera view

--- frontend/AddProduct.js
+++ frontend/AddProduct.js
@@ -1,10 +1,39 @@
-import { SafeAreaView, Button } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { CameraView, Camera } from "expo-camera/next";
+
+import ScanStyles from './ScanStyles'; // Assuming you still need styles
+
+export default function Scan() {
+  const [hasPermission, setHasPermission] = useState(null);
+
+  useEffect(() => {
+    const getCameraPermissions = async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    };
+    getCameraPermissions();
+  }, []);
 
 
-export default AddProduct = ({ navigation }) => {
-    return (
-        <SafeAreaView style={{height: '100%' }}>
-            <Button title="This add products" onPress={() => { navigation.replace("MainView") }} />
-        </SafeAreaView>
-    );
+
+  if (hasPermission === null) {
+    return <Text>No access to camera</Text>;
+  } else if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <CameraView style={StyleSheet.absoluteFillObject} />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+});
---
+++ frontend/ScanStyles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+const ScanStyles = StyleSheet.create({
+    absoluteFillObject: {
+        position: 'absolute',
+
+        width: '100%',
+        height: '100%',
+    },
+});
+
+export default ScanStyles;
