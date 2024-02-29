e989674665cf8ad70b28e4e5692d73d98cca7fe1
Coding-Smurf <correoparaspam2003@gmail.com>
2024-02-29 02:04:22 +0100

    date picker showing on both ios and android

--- frontend/AddProduct.js
+++ frontend/AddProduct.js
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { Alert, Animated, Button, View, Text, } from 'react-native';
+import { Alert, Animated, Button, View, Text,Pressable, Platform } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import ScanStyles from './ScanStyles';
-import { TouchableHighlight } from 'react-native-gesture-handler';
+import { TextInput, TouchableHighlight } from 'react-native-gesture-handler';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import styles from 'react-native-inset-shadow/src/styles';
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
   const [animation] = useState(new Animated.Value(0)); // Initialize animated value
+  const [product, setProduct] = useState(''); // Initialize state variable
+  const [date, setDate] = useState(new Date());
+  const [showPicker, setShowPicker] = useState(false); // State to manage date picker visibility
+
 
   useEffect(() => {
     (async () => {
@@ -22,15 +28,15 @@ export default function App() {
 
   const toggleAnimationHide = () => {
     Animated.timing(animation, {
-      toValue: 0.5, // Toggle between 0 and 1
-      duration: 500, // Animation duration in milliseconds
+      toValue: 0, // Toggle between 0 and 1 
+      duration: 250, // Animation duration in milliseconds
       useNativeDriver: false, // This is required for layout animations
     }).start();
   };
   const toggleAnimationToHalf = () => {
     Animated.timing(animation, {
       toValue: 0.5, // Toggle between 0 and 1
-      duration: 500, // Animation duration in milliseconds
+      duration: 250, // Animation duration in milliseconds
       useNativeDriver: false, // This is required for layout animations
     }).start();
   };
@@ -42,18 +48,35 @@ export default function App() {
     }).start();
   };
 
+  
+  const heightInterpolate = animation.interpolate({
+    inputRange: [0, 0.5, 1],
+    outputRange: ['0%', '50%', '60%'],
+  });
+
   const handleBarCodeScanned = ({ data }) => {
     setScanned(true);
 
     fetch(apiUrl + data + endURL)
       .then((response) => response.json())
       .then((productData) => {
-        const productName = productData.product.product_name || null;
-        const brands = productData.product.brands || null;
-        const imageURL = productData.product.image_url || null;
+        let productName = productData.product.product_name || null;
+        let brands = productData.product.brands || null;
+        let imageURL = productData.product.image_url || null;
+
+        if (productName === null) {
+          productName = ''
+        }
+        if (brands === null) {
+          brands = ''
+        }
+        if (imageURL === null) {
+          imageURL = ''
+        }
 
         //here toggle animation to 50% of the animated.view
         toggleAnimationToHalf();
+        setProduct(productName);
 
       })
       .catch((error) => {
@@ -61,32 +84,68 @@ export default function App() {
       });
   };
 
-  const heightInterpolate = animation.interpolate({
-    inputRange: [0, 0.5, 1],
-    outputRange: ['0%', '50%', '100%'],
-  });
 
+
+
+  const updateUsername = (newValue) => {
+    setProduct(newValue);
+  };
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShowPicker(Platform.OS === 'ios'); // Only close if it's iOS platform
+    setDate(currentDate);
+    setShowPicker(!showPicker);
+  };
+
+  const toggleDatePicker = () => {
+    setShowPicker(!showPicker);
+  };
+
+  //parse date variable into a string
+  let data = date.toDateString();
   return (
     <>
-      <Animated.View style={[ScanStyles.ascendingBox, { height: heightInterpolate }]}>
-
-        
-        
-        <View style={ScanStyles.buttonWrapper}>
-          <TouchableHighlight style={ScanStyles.buttonBox} onPress={() => setScanned(false)}>
-            <Text style={ScanStyles.button}>Save</Text>
-          </TouchableHighlight>
-          <TouchableHighlight style={ScanStyles.buttonBox} onPress={() => setScanned(false) }>
-            <Text style={ScanStyles.button}>Cancel</Text>
-          </TouchableHighlight>
-        </View>
-        
-
-      </Animated.View>
-      <BarCodeScanner
+      
+      <BarCodeScanner 
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
         style={ScanStyles.absoluteFillObject}
       ></BarCodeScanner>
+
+        { showPicker &&
+        <View style={ScanStyles.holePage}>
+          <View style={ScanStyles.calendarIos}>
+            <DateTimePicker
+                  mode = 'date'
+                  display={Platform.OS === 'ios' ? 'inline' : 'default'}
+                  value = {date}
+                  onChange = {onChange}
+            ></DateTimePicker>
+          </View> 
+        </View>
+        }
+ 
+        <Animated.View style={[ScanStyles.ascendingBox, { height: heightInterpolate }]}>
+        <View style={ScanStyles.topPart}>
+          <TextInput style={ScanStyles.name} onFocus={() => toggleAnimationFull()} onBlur={() => toggleAnimationToHalf()} onChangeText={(text) => setProduct(text)} value={product}></TextInput>
+          <View style={ScanStyles.calendarWrap}>
+            <Text style={ScanStyles.calendarText}>{data}</Text>
+            
+            {!showPicker &&
+            <TouchableHighlight style={ScanStyles.calendarButton} onPress={toggleDatePicker}><Text>Open Calendar</Text></TouchableHighlight>}
+          </View>
+        </View>
+
+        <View style={ScanStyles.buttonWrapper}> 
+          <TouchableHighlight underlayColor='rgba(20,20,20,0.25)' style={ScanStyles.buttonBox} onPress={() => {setScanned(false); toggleAnimationFull()}}>
+            <Text style={ScanStyles.button}>Save</Text>
+          </TouchableHighlight>
+          <TouchableHighlight underlayColor='rgba(20,20,20,0.25)' style={ScanStyles.buttonBox} onPress={() => { setScanned(false); toggleAnimationHide() }}>
+            <Text style={ScanStyles.button}>Cancel</Text>
+          </TouchableHighlight>
+        </View>
+      </Animated.View>
+      
     </>
-  );
+  ); 
 }
+
--- frontend/ScanStyles.js
+++ frontend/ScanStyles.js
@@ -1,6 +1,45 @@
 import { StyleSheet } from 'react-native';
 
 const ScanStyles = StyleSheet.create({
+    topPart:{
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+    },
+    name:{ 
+        width: '80%',
+        height: 50,
+        padding: 10,
+        borderColor: 'black',
+        borderWidth: 1, 
+        borderRadius: 10,
+    },
+
+    calendarWrap:{
+        flexDirection: 'row', alignItems: 'center', justifyContent: 'center'
+
+    },
+    holePage:{
+        width: '100%',
+        height: '100%',
+        //center elements vertically and horizontally
+        justifyContent: 'center',
+        alignItems: 'center',
+        zIndex: 1,
+
+    },
+    calendarIos:{
+        center: 'center', 
+        backgroundColor: 'white',
+        borderRadius: 20,
+        borderColor: 'black',
+        borderWidth: 1,
+        color: 'white',
+        padding: 20,
+        
+    },
+
+
     absoluteFillObject: {
         position: 'absolute',
         width: '100%',
@@ -12,8 +51,8 @@ const ScanStyles = StyleSheet.create({
         width: '100%',
         height: '100%',
         backgroundColor: 'white',
-        zIndex: 1,
         borderRadius: 40,
+        paddingTop: 30,
     },
     buttonWrapper:{
         
@@ -21,22 +60,50 @@ const ScanStyles = StyleSheet.create({
         
         justifyContent: 'center',
         
-        backgroundColor: 'black',
     },
     buttonBox:{
-       margin: 20,
-       width: 100,
-       height: 50,
-       justifyContent: 'center',
-       alignItems: 'center',
-       backgroundColor: 'blue',
+        margin: 20,
+        width: 100,
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        borderRadius: 10, 
+        borderColor: 'black',
+        borderWidth: 1, 
+        //when touched the button will change color
+        shadowColor: 'black',
+        shadowOffset: {width: 0, height: 2},
+        shadowOpacity: 0.5,
+        shadowRadius: 2,
+        elevation: 5,
+
+
     },
     button:{
 
         justifyContent: 'center',
         alignItems: 'center',
-        color: 'red',
-    }
+        
+    },
+
+    calendarText:{
+        width: '40%',
+        margin: 20,
+        padding: 15,
+        borderColor: 'black',
+        borderWidth: 1,
+        borderRadius: 10,
+    },
+    
+    calendarButton:{
+        width: '100%',
+        borderColor: 'black',
+        borderWidth: 1,
+        borderRadius: 10,
+    },
+
+
 });
 
 export default ScanStyles;
