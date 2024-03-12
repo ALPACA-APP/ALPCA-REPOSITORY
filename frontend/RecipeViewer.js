c312acd8913364fd945f4306a728c72030a70b9a
Coding-Smurf <correoparaspam2003@gmail.com>
2024-03-12 23:24:22 +0100

    changed the props to not send the uuid but the hole user object in RecipeViewer

--- frontend/RecipeViewer.js
+++ frontend/RecipeViewer.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import RecipeViewerStyles from './RecipeViewerStyles';
 
 export default RecipeViewer = ({ route }) => {
-    const { recipeId, uuid } = route.params;
+    const { recipeId, userObject } = route.params;
 
     return (
         <View>
             <Text>RecipeViewer</Text>
             <Text>Recipe ID: {recipeId}</Text>
-            <Text>UUID: {uuid}</Text>
+            <Text>User UUID: {userObject.uuid}</Text>
+            
         </View>
     );
 }
---
+++ frontend/RecipeViewerStyles.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from 'react-native';
+
+const RecipeViewerStyles = StyleSheet.create({
+
+
+});
+
+export default RecipeViewerStyles;
