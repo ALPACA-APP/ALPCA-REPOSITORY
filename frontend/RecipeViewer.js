22e51ce3bfc8b308828b9a449b17b6b650448521
Coding-Smurf <correoparaspam2003@gmail.com>
2024-03-12 23:36:25 +0100

    added functional header in RecipeViewer

--- frontend/RecipeViewer.js
+++ frontend/RecipeViewer.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, SafeAreaView } from 'react-native';
+import navigation from '@react-navigation/native';
+import Header from "./Header";
 import RecipeViewerStyles from './RecipeViewerStyles';
 
-export default RecipeViewer = ({ route }) => {
+export default RecipeViewer = ({ route, navigation }) => {
     const { recipeId, userObject } = route.params;
 
+    const goBack = () => {
+        navigation.navigate('Recipes View');
+    }
+
     return (
-        <View>
-            <Text>RecipeViewer</Text>
-            <Text>Recipe ID: {recipeId}</Text>
-            <Text>User UUID: {userObject.uuid}</Text>
+        <SafeAreaView style={{ height: '100%',  backgroundColor:'white'}}>
             
-        </View>
+            <Header isLogout="false" onGoBack={() => { goBack(); }} userObject={userObject} />
+            <View>
+                <Text>RecipeViewer</Text>
+                <Text>Recipe ID: {recipeId}</Text>
+                <Text>User UUID: {userObject.uuid}</Text>
+            </View>
+            
+            
+        </SafeAreaView>
     );
 }
